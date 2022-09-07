4abc7b1ecd6805af5a6b5a32c436c3b007008261
Vadzim Belski <belski@scnsoft.com>
2022-09-07 18:47:15 +0300

    Added Ethereum scanner

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 extern crate serde_json;
 extern crate ticker;
+extern crate bitcoin;
+extern crate hex;
 
+use web3::signing::keccak256;
 use std::time::Duration;
 
 use web3::{
-    types::{ TransactionId,BlockId},
+    types::{ TransactionId, BlockId},
 };
+
+use secp256k1::rand::rngs::OsRng;
+use secp256k1::{Secp256k1};
+use web3::types::Address;
+use std::str::FromStr;
+
+
+use bip0039::{Count, Mnemonic};
+use bitcoin::secp256k1::ffi::types::AlignedType;
+use bitcoin::util::bip32::{ChildNumber, DerivationPath, ExtendedPrivKey, ExtendedPubKey};
+use bitcoin::PublicKey;
 
 use ticker::Ticker;
 
 #[tokio::main]
 async fn main() -> web3::Result {
+
+    generate_keypair();
     // Sign up at infura > choose the desired network (eg Rinkeby) > copy the endpoint url into the below
     // If you need test ether use a faucet, eg https://faucet.rinkeby.io/
     let transport = web3::transports::Http::new("https://ropsten.infura.io/v3/f1a6a5d57420473b975975c55f5d3666")?;
@@ -85,13 +101,13 @@ async fn main() -> web3::Result {
     Ok(())
 }
 
-async fn consume(block_id : web3::types::BlockId, web3http : &web3::Web3<web3::transports::Http>) {
+// async fn consume(block_id : web3::types::BlockId, web3http : &web3::Web3<web3::transports::Http>) {
 
-    let block = web3http.eth().block(block_id).await;
-    let block_object = block.as_ref().unwrap();
+//     let block = web3http.eth().block(block_id).await;
+//     let block_object = block.as_ref().unwrap();
 
-    println!("Block: {:?}", block_object);
-}
+//     println!("Block: {:?}", block_object);
+// }
 
 async fn scan_transaction( tx: web3::types::H256, web3http: &web3::Web3<web3::transports::Http>){
 
@@ -101,4 +117,52 @@ async fn scan_transaction( tx: web3::types::H256, web3http: &web3::Web3<web3::tr
 
     println!("Addres From: {:?}", transaction.from);
     println!("Addres to: {:?}", transaction.to);
+}
+
+fn generate_keypair() {
+    
+    let network = bitcoin::Network::Bitcoin;
+
+    // Generates an English mnemonic with 12 words randomly
+    //let mnemonic = Mnemonic::generate(Count::Words12);
+    let mnemonic = Mnemonic::from_phrase("jealous picnic lazy lend basic kangaroo debate inspire select brisk neither license").unwrap();
+    // Gets the phrase
+    let _phrase = mnemonic.phrase();
+
+    println!("Phrase generated: {}", _phrase);
+
+    // Generates the HD wallet seed from the mnemonic and the passphrase.
+    let seed = mnemonic.to_seed("");
+
+    // we need secp256k1 context for key derivation
+    let mut buf: Vec<AlignedType> = Vec::new();
+    buf.resize(Secp256k1::preallocate_size(), AlignedType::zeroed());
+    let secp = Secp256k1::preallocated_new(buf.as_mut_slice()).unwrap();
+
+    // calculate root key from seed
+    let root = ExtendedPrivKey::new_master(network, &seed).unwrap();
+    println!("Root key: {}", root);
+
+    println!("Root hex: {}", hex::encode(root.to_priv().to_bytes()));
+
+    // derive child xpub
+    let path = DerivationPath::from_str("m/44'/60'/0'/0/0").unwrap();
+    let child = root.derive_priv(&secp, &path).unwrap();
+    println!("Child at {}: {}", path, child);
+
+    println!("Child private hex: {}", hex::encode(child.to_priv().to_bytes()));
+
+
+    let xpub = ExtendedPubKey::from_priv(&secp, &child);
+    println!("Public key at {}: {}", path, xpub);
+
+
+    let public_key = xpub.public_key;
+
+    let public_key = public_key.serialize_uncompressed();
+    let hash = keccak256(&public_key[1..]);
+
+    let address = Address::from_slice(&hash[12..]);
+
+    println!("address: {:?}", &address);
 }
