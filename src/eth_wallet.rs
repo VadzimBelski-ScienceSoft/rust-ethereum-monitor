48764ede8ba50c70e6e314a21a4c0d7e2db21b60
ybely <ybely@scnsoft.com>
2022-09-14 14:11:32 +0300

    #1 Add wallet

---
+++ src/eth_wallet.rs
@@ -0,0 +1,109 @@
+use crate::utils;
+use anyhow::Result;
+use secp256k1::{PublicKey, SecretKey}; 
+use secp256k1::rand::rngs::OsRng;
+use serde::{Deserialize, Serialize};
+use std::io::BufWriter;
+use std::str::FromStr;
+use std::{fs::OpenOptions, io::BufReader};
+use web3::{
+    transports,
+    signing::keccak256,
+    types::{Address, TransactionParameters, U256},
+    Web3,
+};
+
+pub fn generate_keypair() -> (SecretKey, PublicKey) {
+    let secp = secp256k1::Secp256k1::new();
+    secp.generate_keypair(&mut OsRng)
+}
+
+pub fn public_key_address(public_key: &PublicKey) -> Address {
+    let public_key = public_key.serialize_uncompressed();
+    debug_assert_eq!(public_key[0], 0x04);
+    let hash = keccak256(&public_key[1..]);
+
+    Address::from_slice(&hash[12..])
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Wallet {
+    pub secret_key: String,
+    pub public_key: String,
+    pub public_address: String,
+}
+
+impl Wallet {
+    #[allow(dead_code)]
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let addr: Address = public_key_address(&public_key);
+        Wallet {
+            secret_key: format!("{}", secret_key.display_secret()),
+            public_key: public_key.to_string(),
+            public_address: format!("{:?}", addr),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn save_to_file(&self, file_path: &str) -> Result<()> {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(file_path)?;
+        let buf_writer = BufWriter::new(file);
+
+        serde_json::to_writer_pretty(buf_writer, self)?;
+
+        Ok(())
+    }
+
+    pub fn from_file(file_path: &str) -> Result<Wallet> {
+        let file = OpenOptions::new().read(true).open(file_path)?;
+        let buf_reader = BufReader::new(file);
+
+        let wallet: Wallet = serde_json::from_reader(buf_reader)?;
+        Ok(wallet)
+    }
+
+    #[allow(dead_code)]
+    pub fn get_secret_key(&self) -> Result<SecretKey> {
+        let secret_key = SecretKey::from_str(&self.secret_key)?;
+        Ok(secret_key)
+    }
+
+    #[allow(dead_code)]
+    pub fn get_public_key(&self) -> Result<PublicKey> {
+        let pub_key = PublicKey::from_str(&self.public_key)?;
+        Ok(pub_key)
+    }
+
+    pub async fn get_balance(&self, web3_connection: &Web3<transports::WebSocket>) -> Result<U256> {
+        let wallet_address = Address::from_str(&self.public_address)?;
+        let balance = web3_connection.eth().balance(wallet_address, None).await?;
+
+        Ok(balance)
+    }
+
+    pub async fn get_balance_in_eth(
+        &self,
+        web3_connection: &Web3<transports::WebSocket>,
+    ) -> Result<f64> {
+        let wei_balance = self.get_balance(web3_connection).await?;
+        Ok(utils::wei_to_eth(wei_balance))
+    }
+}
+
+pub async fn establish_web3_connection(url: &str) -> Result<Web3<transports::WebSocket>> {
+    let transport = transports::WebSocket::new(url).await?;
+    Ok(Web3::new(transport))
+}
+
+#[allow(dead_code)]
+pub fn create_eth_transaction(to: Address, eth_value: f64) -> TransactionParameters {
+    TransactionParameters {
+        to: Some(to),
+        value: utils::eth_to_wei(eth_value),
+        ..Default::default()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,168 +1,43 @@
-extern crate serde_json;
-extern crate ticker;
-extern crate bitcoin;
-extern crate hex;
-
-use web3::signing::keccak256;
-use std::time::Duration;
-
-use web3::{
-    types::{ TransactionId, BlockId},
-};
-
-use secp256k1::rand::rngs::OsRng;
-use secp256k1::{Secp256k1};
-use web3::types::Address;
-use std::str::FromStr;
-
-
-use bip0039::{Count, Mnemonic};
-use bitcoin::secp256k1::ffi::types::AlignedType;
-use bitcoin::util::bip32::{ChildNumber, DerivationPath, ExtendedPrivKey, ExtendedPubKey};
-use bitcoin::PublicKey;
-
-use ticker::Ticker;
+use anyhow::Result;
+mod eth_wallet;
+mod utils;
+use std::env;
 
 #[tokio::main]
-async fn main() -> web3::Result {
+async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+    let (secret_key, pub_key) = eth_wallet::generate_keypair();
 
-    generate_keypair();
-    // Sign up at infura > choose the desired network (eg Rinkeby) > copy the endpoint url into the below
-    // If you need test ether use a faucet, eg https://faucet.rinkeby.io/
-    let transport = web3::transports::Http::new("https://ropsten.infura.io/v3/f1a6a5d57420473b975975c55f5d3666")?;
-    let web3http = web3::Web3::new(transport);
+    println!("secret key: {}", &secret_key.display_secret());
+    println!("public key: {}", &pub_key.to_string());
 
-    let ws = web3::transports::WebSocket::new("wss://ropsten.infura.io/ws/v3/f1a6a5d57420473b975975c55f5d3666").await?;
-    let web3 = web3::Web3::new(ws.clone());
+    let pub_address = eth_wallet::public_key_address(&pub_key);
+    println!("public address: {:?}", pub_address);
 
-    let mut sub = web3.eth_subscribe().subscribe_new_heads().await?;
+    let crypto_wallet = eth_wallet::Wallet::new(&secret_key, &pub_key);
+    println!("crypto_wallet: {:?}", &crypto_wallet);
 
-    println!("Got subscription id: {:?}", sub.id());
+    let wallet_file_path = "crypto_wallet.json";
+    crypto_wallet.save_to_file(wallet_file_path)?;
 
-    // future::ready(&mut sub).then(|x| async move{
-            
-    //         println!("Got: {:?}", x);
+    let loaded_wallet = eth_wallet::Wallet::from_file(wallet_file_path)?;
+    println!("loaded_wallet: {:?}", loaded_wallet);
 
-    //         // let result = x;
+    let endpoint = env::var("INFURA_RINKEBY_WS")?;
+    let web3_con = eth_wallet::establish_web3_connection(&endpoint).await?;
 
-    //         // let block = web3.eth().block(BlockId::from(result.number.unwrap())).await.unwrap();
-    //         // let block_object = block.as_ref().unwrap();
+    let block_number = web3_con.eth().block_number().await?;
+    println!("block number: {}", &block_number);
 
-    //         future::ready(())
-    //     });
+    let balance = loaded_wallet.get_balance_in_eth(&web3_con).await?;
+    println!("wallet balance: {} eth", &balance);
 
+    // let transaction =
+    //     eth_wallet::create_eth_transaction(Address::from_str("0x4fill in address here")?, 0.01);
+    // let transact_hash =
+    //     eth_wallet::sign_and_send(&web3_con, transaction, &loaded_wallet.get_secret_key()?).await?;
 
-    // while let Some(x) = sub.next().await {
-    //     consume(BlockId::from(x.unwrap().number.unwrap()), &web3http).await;
-    // }
-
-    // sub.unsubscribe().await?;
-
-
-    let current_block_number = web3.eth().block_number().await;
-
-    let mut next_block_number = current_block_number.unwrap();
-    let mut latest_scanned_block = web3::types::U64::from(0);
-
-    let ticker = Ticker::new(0.., Duration::from_secs(5));
-
-    for _ in ticker {
- 
-        println!("We are on the block {:?}", next_block_number);
-
-        let block = web3.eth().block(BlockId::from(next_block_number)).await.unwrap();
-
-        println!("Block details are {:?}", block);
-
-        if block != None {
-
-            let block_object = block.as_ref().unwrap();
-        
-            if latest_scanned_block != block_object.number.unwrap() {
-                for tx in &block_object.transactions {
-                    scan_transaction(*tx, &web3http).await;
-                    println!("Transaction {:?}", tx);
-                    latest_scanned_block = block_object.number.unwrap();
-                }
-            }
-
-            let increment = 1 as u64;
-            next_block_number = block_object.number.unwrap() + increment;
-            println!("Setting next block as {:?}", next_block_number);
-            
-
-        }else{
-            println!("No more blocks");
-        }            
-    
-    }
-
+    // println!("transaction hash: {:?}", transact_hash);
 
     Ok(())
-}
-
-// async fn consume(block_id : web3::types::BlockId, web3http : &web3::Web3<web3::transports::Http>) {
-
-//     let block = web3http.eth().block(block_id).await;
-//     let block_object = block.as_ref().unwrap();
-
-//     println!("Block: {:?}", block_object);
-// }
-
-async fn scan_transaction( tx: web3::types::H256, web3http: &web3::Web3<web3::transports::Http>){
-
-    let tr = web3http.eth().transaction(TransactionId::Hash(tx)).await.unwrap();
-
-    let transaction = tr.as_ref().unwrap();
-
-    println!("Addres From: {:?}", transaction.from);
-    println!("Addres to: {:?}", transaction.to);
-}
-
-fn generate_keypair() {
-    
-    let network = bitcoin::Network::Bitcoin;
-
-    // Generates an English mnemonic with 12 words randomly
-    //let mnemonic = Mnemonic::generate(Count::Words12);
-    let mnemonic = Mnemonic::from_phrase("jealous picnic lazy lend basic kangaroo debate inspire select brisk neither license").unwrap();
-    // Gets the phrase
-    let _phrase = mnemonic.phrase();
-
-    println!("Phrase generated: {}", _phrase);
-
-    // Generates the HD wallet seed from the mnemonic and the passphrase.
-    let seed = mnemonic.to_seed("");
-
-    // we need secp256k1 context for key derivation
-    let mut buf: Vec<AlignedType> = Vec::new();
-    buf.resize(Secp256k1::preallocate_size(), AlignedType::zeroed());
-    let secp = Secp256k1::preallocated_new(buf.as_mut_slice()).unwrap();
-
-    // calculate root key from seed
-    let root = ExtendedPrivKey::new_master(network, &seed).unwrap();
-    println!("Root key: {}", root);
-
-    println!("Root hex: {}", hex::encode(root.to_priv().to_bytes()));
-
-    // derive child xpub
-    let path = DerivationPath::from_str("m/44'/60'/0'/0/0").unwrap();
-    let child = root.derive_priv(&secp, &path).unwrap();
-    println!("Child at {}: {}", path, child);
-
-    println!("Child private hex: {}", hex::encode(child.to_priv().to_bytes()));
-
-
-    let xpub = ExtendedPubKey::from_priv(&secp, &child);
-    println!("Public key at {}: {}", path, xpub);
-
-
-    let public_key = xpub.public_key;
-
-    let public_key = public_key.serialize_uncompressed();
-    let hash = keccak256(&public_key[1..]);
-
-    let address = Address::from_slice(&hash[12..]);
-
-    println!("address: {:?}", &address);
 }
